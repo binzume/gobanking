6d065493aedd62d6eaef14fc47a3150b4dad5569
Kousuke Kawahira <kawahira@binzume.net>
2018-01-07 07:23:13 +0900

    more better stub.

--- stub/dummy.go
+++ stub/dummy.go
@@ -7,14 +7,20 @@ import (
 )
 
 type Account struct {
+	common.BankAccount
+	balance int64
 }
 
 const BankCode = "9999"
+const BankName = "テスト銀行"
 
 var _ common.Account = &Account{}
 
 func Login(id, password string) (*Account, error) {
-	a := &Account{}
+	a := &Account{
+		common.BankAccount{BankCode: BankCode, BankName: BankName, BranchCode: "001", BranchName: "テスト支店", OwnerName: id},
+		123456712345678,
+	}
 	err := a.Login(id, password, nil)
 	return a, err
 }
@@ -28,7 +34,7 @@ func (a *Account) Logout() error {
 }
 
 func (a *Account) TotalBalance() (int64, error) {
-	return 0, nil
+	return a.balance, nil
 }
 
 func (a *Account) LastLogin() (time.Time, error) {
@@ -36,16 +42,21 @@ func (a *Account) LastLogin() (time.Time, error) {
 }
 
 func (a *Account) Recent() ([]*common.Transaction, error) {
-	return nil, nil
+	base := time.Now().Truncate(time.Hour * 24).Add(-time.Hour * 24 * 7) // week ago today.
+	return []*common.Transaction{
+		&common.Transaction{Date: base, Amount: 123, Balance: 123, Description: "test"},
+		&common.Transaction{Date: base.Add(time.Hour * 48), Amount: 10000, Balance: 10123, Description: "test2"},
+		&common.Transaction{Date: time.Now().Truncate(time.Second), Amount: -5000, Balance: 5123, Description: "test..."},
+	}, nil
 }
 
 func (a *Account) History(from, to time.Time) ([]*common.Transaction, error) {
-	return nil, nil
+	return a.Recent()
 }
 
 // transfar api
 func (a *Account) NewTransactionWithNick(targetName string, amount int64) (common.TempTransaction, error) {
-	return nil, nil
+	return common.TempTransactionMap{"fee": 100, "amount": amount, "to": targetName}, nil
 }
 
 func (a *Account) CommitTransaction(tr common.TempTransaction, pass2 string) (string, error) {
